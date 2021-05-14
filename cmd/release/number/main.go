8db3e8fa0d94e40388fdd95d9116ca56a4ac92b5
Kirsten Schumy <ksschumy@amazon.com>
2021-05-13 23:52:29 -0700

    working! hopefully

--- cmd/release/number/main.go
+++ cmd/release/number/main.go
@@ -83,8 +83,8 @@ func main() {
 	//if !c.dryRun {
 	err = cmdTwo.Run()
 	if err != nil {
-		resetPath := "checkout HEAD^ -- " + release.EnvironmentReleasePath
-		exec.Command("git", resetPath)
+		//resetPath := "checkout HEAD^ -- " + release.EnvironmentReleasePath
+		//exec.Command("git", resetPath)
 		log.Fatalf("Error running make: %v", err)
 
 	}
