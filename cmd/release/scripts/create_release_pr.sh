14d55dc6f5bfa2f154142eee883d179fbd75092b
Kirsten Schumy <ksschumy@amazon.com>
2021-05-13 22:31:01 -0700

    working! hopefully

--- cmd/release/scripts/create_release_pr.sh
+++ cmd/release/scripts/create_release_pr.sh
@@ -63,6 +63,10 @@ echo $PR_BRANCH
 echo "pushing?"
 
 PR_EXISTS=$(gh pr list | grep -c "${PR_BRANCH}" || true)
+  echo "PR_EXISTS?"
+  echo $PR_EXISTS
+
 if [ "${PR_EXISTS}" -eq 0 ]; then
+    echo "INSIDE"
   gh pr create --title "${PR_TITLE}" --body "${PR_BODY}" --web --repo "aws/eks-distro"
 fi
