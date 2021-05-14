00c4393907f5e5776b60e087e8c3c12364592125
Kirsten Schumy <ksschumy@amazon.com>
2021-05-13 23:52:29 -0700

    working! hopefully

--- cmd/release/scripts/create_release_pr.sh
+++ cmd/release/scripts/create_release_pr.sh
@@ -21,65 +21,54 @@ RELEASE_FILEPATH="${1?....}"
 RELEASE_ENVIRONMENT="${2?Should be 'development' or 'production'}"
 RELEASE_VERSION="${3?Release branch}"
 
+ORIGINAL_BRANCH=$(git branch --show-current)
+PR_BRANCH="increment-${RELEASE_ENVIRONMENT}-RELEASE-${RELEASE_VERSION}" #"automated-release-update" #"increment-development-RELEASE-1.19-28"
+
 function cleanup {
   echo "TRAPPPPPPPED"
-#  rm  -r /tmp/foo
+  git checkout HEAD^ -- "${RELEASE_FILEPATH}"
+  git co $ORIGINAL_BRANCH
+  #  rm  -r /tmp/foo
 }
 
 trap cleanup ERR
 
 echo "hellooo"
 
-#IS_BOT=false
+ORIGIN_ORG=$(git remote get-url origin | sed -n -e "s|git@github.com:\(.*\)/eks-distro.git|\1|p")
+
+PR_TITLE="Increment ${RELEASE_ENVIRONMENT} RELEASE for ${RELEASE_VERSION}"
+COMMIT_MESSAGE="TEST!! [PR BOT] Increment RELEASE for"
 #
-#ORIGIN_ORG=$(git remote get-url origin | sed -n -e "s|git@github.com:\(.*\)/eks-distro.git|\1|p")
-#
-#PR_TITLE="Increment ${RELEASE_ENVIRONMENT} RELEASE for ${RELEASE_VERSION}"
-#COMMIT_MESSAGE="TEST!! [PR BOT] Increment RELEASE for"
-##
-#PR_BODY=$(
-#  cat <<EOF
-#TEST!! Bumping RELEASE version
-#
-#By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.
-#EOF
-#)
-##
-#PR_BRANCH="increment-${RELEASE_ENVIRONMENT}-RELEASE-${RELEASE_VERSION}" #"automated-release-update" #"increment-development-RELEASE-1.19-28"
-#
-#echo $PR_BRANCH
-#
-#git checkout -B $PR_BRANCH
-#
-#if [[ "$(git status --porcelain | wc -l)" -eq 1 ]]; then
-#  git add "${RELEASE_FILEPATH}"
-#  if [[ $(git diff --staged --name-only) == "" ]]; then
-#    exit 0
-#  fi
-#  git commit -m "${COMMIT_MESSAGE}" || true
-#
-##  git fetch upstream
-##  # there will be conflicts before we are on the bots fork at this point
-##  # -Xtheirs instructs git to favor the changes from the current branch
-##  git rebase -Xtheirs upstream/main
-#else
-#  git restore "${RELEASE_FILEPATH}"
-#  echo "Unexpected files."
-#  echo "Restored ${RELEASE_FILEPATH}"
-#  exit 1
-#fi
-#
-#echo "pushing..."
-#git push origin ${PR_BRANCH}
-#
-##echo $PR_BRANCH
-#echo "pushing?"
-#
-#PR_EXISTS=$(gh pr list | grep -c "${PR_BRANCH}" || true)
-#echo "PR_EXISTS?"
-#echo $PR_EXISTS
-#
-#if [ "${PR_EXISTS}" -eq 0 ]; then
-#  echo "INSIDE"
-#  gh pr create --title "${PR_TITLE}" --body "${PR_BODY}" --web --repo "aws/eks-distro"
-#fi
+PR_BODY=$(
+  cat <<EOF
+TEST!! Bumping RELEASE version
+
+By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.
+EOF
+)
+
+git checkout -B $PR_BRANCH
+
+if [[ "$(git status --porcelain | wc -l)" -eq 1 ]]; then
+  git add "${RELEASE_FILEPATH}"
+  if [[ $(git diff --staged --name-only) == "" ]]; then
+    exit 0
+  fi
+  git commit -m "${COMMIT_MESSAGE}" || true
+else
+  git restore "${RELEASE_FILEPATH}"
+  echo "Unexpected files."
+  echo "Restored ${RELEASE_FILEPATH}"
+  exit 1
+fi
+
+echo "pushing..."
+git push origin ${PR_BRANCH}
+echo "pushed!"
+
+PR_EXISTS=$(gh pr list | grep -c "${PR_BRANCH}" || true)
+if [ "${PR_EXISTS}" -eq 0 ]; then
+  echo "INSIDE"
+  gh pr create --title "${PR_TITLE}" --body "${PR_BODY}" --web --repo "aws/eks-distro"
+fi
